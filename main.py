2b3a86140b1b8b97f4e5386c4c4e6250eecc0f9b
Your Name <70295002+moritz-john@users.noreply.github.com>
2022-12-22 13:08:19 +0100

    OLED implementation

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import board
 from kb import KMKKeyboard
 
 from kmk.keys import KC
-from kmk.modules.split import Split, SplitType
 from kmk.modules.layers import Layers
 from kmk.extensions.media_keys import MediaKeys 
+from kmk.modules.split import Split, SplitType
+from kmk.extensions.peg_oled_Display import Oled,OledDisplayMode,OledReactionType,OledData
 
 keyboard = KMKKeyboard()
 layers_ext = Layers()
@@ -20,14 +21,29 @@ split = Split(
     use_pio=True,  # Use RP2040 PIO implementation of UART. Required if you want to use other pins than RX/TX
 )
 
+keyboard.modules = [split, layers_ext,]
+keyboard.extensions = [media,]
 
-keyboard.modules = [split, layers_ext]
-keyboard.extensions = [media]
+
+# Comment this block out if you are not using OLEDs
+oled_ext = Oled(
+    OledData(
+        corner_one={0:OledReactionType.STATIC,1:["Layer"]},
+        corner_two={0:OledReactionType.LAYER,1:["1","2","3","4"]},
+        corner_three={0:OledReactionType.LAYER,1:["BASE","LOWER","RAISE","ADJUST"]},
+        corner_four={0:OledReactionType.LAYER,1:["qwerty","nums","shifted","leds"]}
+        ),
+        toDisplay=OledDisplayMode.TXT,
+        flip=True,
+)
+keyboard.extensions.append(oled_ext) 
+
 
 # Key aliases
 xxxxxxx = KC.NO
 _______ = KC.TRNS
 LOWER = KC.MO(1)
+RAISE = KC.MO(2)
 
 # Keymap
 keyboard.keymap = [
@@ -37,7 +53,7 @@ keyboard.keymap = [
                   KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,                        KC.Y,    KC.U,    KC.I,    KC.O,    KC.P,         \
         KC.N1,    KC.A,    KC.S,    KC.D,    KC.F,    KC.G,                        KC.H,    KC.J,    KC.K,    KC.L, KC.SCLN, KC.N2,  \
         KC.N2,    KC.Z,    KC.X,    KC.C,    KC.V,    KC.B, KC.MUTE,   KC.MPLY,    KC.N,    KC.M, KC.COMM,  KC.DOT, KC.SLSH, KC.N3,  \
-                          KC.N1,   LOWER,   KC.N3,   KC.N4,                       KC.N4,   KC.N3,  KC.SPC,   KC.N1,  
+                          KC.N1,   LOWER,   KC.N3,   KC.N4,                       KC.N4,   KC.N3,   RAISE,   KC.N1,  
         
         # Encoders
         KC.AUDIO_VOL_UP,     #Left side counterclockwise                                                                  
@@ -51,6 +67,13 @@ keyboard.keymap = [
                KC.N1,    KC.A,    KC.S,    KC.D,    KC.F,    KC.G,                        KC.H,    KC.J,    KC.K,    KC.L, KC.SCLN, KC.N2,  \
                KC.N2,    KC.Z,    KC.X,    KC.C,    KC.V,    KC.B, KC.MPRV,   KC.MNXT,    KC.N,    KC.M, KC.COMM,  KC.DOT, KC.SLSH, KC.N3,  \
                                  KC.N1, _______,   KC.N3,   KC.N4,                       KC.N4,   KC.N3,  KC.SPC,   KC.N1,  
+    ],
+    [  
+       #RAISE       |        |        |        |        |        |        | |        |        |        |        |        |        |        |
+                         KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,                        KC.Y,    KC.U,    KC.I,    KC.O,    KC.P,         \
+               KC.N1,    KC.A,    KC.S,    KC.D,    KC.F,    KC.G,                        KC.H,    KC.J,    KC.K,    KC.L, KC.SCLN, KC.N2,  \
+               KC.N2,    KC.Z,    KC.X,    KC.C,    KC.V,    KC.B, KC.MPRV,   KC.MNXT,    KC.N,    KC.M, KC.COMM,  KC.DOT, KC.SLSH, KC.N3,  \
+                                 KC.N1, _______,   KC.N3,   KC.N4,                       KC.N4,   KC.N3, _______,   KC.N1,  
     ],
 ]
 
