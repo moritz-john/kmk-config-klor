14057ee0fb19f905860df732b99715def1e675e4
Your Name <70295002+moritz-john@users.noreply.github.com>
2022-12-26 11:02:35 +0100

    First buzzer try

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import board
+import digitalio
 
+import pwmio
+import time
 from kb import KMKKeyboard
 
 from kmk.keys import KC
 from kmk.modules.layers import Layers
-from kmk.extensions.media_keys import MediaKeys 
+from kmk.extensions.media_keys import MediaKeys
 from kmk.modules.split import Split, SplitType, SplitSide
 from kmk.extensions.peg_oled_Display import Oled,OledDisplayMode,OledReactionType,OledData
 
@@ -37,8 +40,20 @@ oled_ext = Oled(
         toDisplay=OledDisplayMode.TXT,
         flip=True,
 )
-keyboard.extensions.append(oled_ext) 
+keyboard.extensions.append(oled_ext)
 # OLED code ends here ---
+
+if split.split_side == SplitSide.LEFT:
+    buzzer = pwmio.PWMOut(board.D21, variable_frequency=True)
+    OFF = 0
+    ON = 2**15
+    buzzer.duty_cycle = ON
+    buzzer.frequency = 2000
+    time.sleep(0.2)
+    buzzer.frequency = 1000
+    time.sleep(0.2)
+    buzzer.duty_cycle = OFF
+
 
 # Key aliases
 xxxxxxx = KC.NO
@@ -48,43 +63,43 @@ RAISE = KC.MO(2)
 
 # Keymap
 keyboard.keymap = [
-    [  
-       #BASE 
+    [
+       #BASE
        #     |        |        |        |        |        |        | |        |        |        |        |        |        |        |
                   KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,                        KC.Y,    KC.U,    KC.I,    KC.O,    KC.P,         \
         KC.N1,    KC.A,    KC.S,    KC.D,    KC.F,    KC.G,                        KC.H,    KC.J,    KC.K,    KC.L, KC.SCLN, KC.N2,  \
         KC.N2,    KC.Z,    KC.X,    KC.C,    KC.V,    KC.B, KC.MUTE,   KC.MPLY,    KC.N,    KC.M, KC.COMM,  KC.DOT, KC.SLSH, KC.N3,  \
-                          KC.N1,   LOWER,   KC.N3,   KC.N4,                       KC.N4,   KC.N3,   RAISE,   KC.N1,  
-        
+                          KC.N1,   LOWER,   KC.N3,   KC.N4,                       KC.N4,   KC.N3,   RAISE,   KC.N1,
+
         # Encoders
-        KC.AUDIO_VOL_UP,     #Left side counterclockwise                                                                  
-        KC.AUDIO_VOL_DOWN,   #Left side clockwise 
-        KC.MEDIA_PREV_TRACK, #Right side counterclockwise                                                          
-        KC.MEDIA_NEXT_TRACK, #Right side clockwise 
+        KC.AUDIO_VOL_UP,     #Left side counterclockwise
+        KC.AUDIO_VOL_DOWN,   #Left side clockwise
+        KC.MEDIA_PREV_TRACK, #Right side counterclockwise
+        KC.MEDIA_NEXT_TRACK, #Right side clockwise
     ],
-    [  
+    [
        #LOWER       |        |        |        |        |        |        | |        |        |        |        |        |        |        |
                          KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,                        KC.Y,    KC.U,    KC.I,    KC.O,    KC.P,         \
                KC.N1,    KC.A,    KC.S,    KC.D,    KC.F,    KC.G,                        KC.H,    KC.J,    KC.K,    KC.L, KC.SCLN, KC.N2,  \
                KC.N2,    KC.Z,    KC.X,    KC.C,    KC.V,    KC.B, KC.MPRV,   KC.MNXT,    KC.N,    KC.M, KC.COMM,  KC.DOT, KC.SLSH, KC.N3,  \
-                                 KC.N1, _______,   KC.N3,   KC.N4,                       KC.N4,   KC.N3,  KC.SPC,   KC.N1,  
+                                 KC.N1, _______,   KC.N3,   KC.N4,                       KC.N4,   KC.N3,  KC.SPC,   KC.N1,
         # Encoders
-        KC.AUDIO_VOL_UP,     #Left side counterclockwise                                                                  
-        KC.AUDIO_VOL_DOWN,   #Left side clockwise 
-        KC.MEDIA_PREV_TRACK, #Right side counterclockwise                                                          
-        KC.MEDIA_NEXT_TRACK, #Right side clockwise 
+        KC.AUDIO_VOL_UP,     #Left side counterclockwise
+        KC.AUDIO_VOL_DOWN,   #Left side clockwise
+        KC.MEDIA_PREV_TRACK, #Right side counterclockwise
+        KC.MEDIA_NEXT_TRACK, #Right side clockwise
     ],
-    [  
+    [
        #RAISE       |        |        |        |        |        |        | |        |        |        |        |        |        |        |
                          KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,                        KC.Y,    KC.U,    KC.I,    KC.O,    KC.P,         \
                KC.N1,    KC.A,    KC.S,    KC.D,    KC.F,    KC.G,                        KC.H,    KC.J,    KC.K,    KC.L, KC.SCLN, KC.N2,  \
                KC.N2,    KC.Z,    KC.X,    KC.C,    KC.V,    KC.B, KC.MPRV,   KC.MNXT,    KC.N,    KC.M, KC.COMM,  KC.DOT, KC.SLSH, KC.N3,  \
                                  KC.N1, _______,   KC.N3,   KC.N4,                       KC.N4,   KC.N3, _______,   KC.N1,
         # Encoders
-        KC.AUDIO_VOL_UP,     #Left side counterclockwise                                                                  
-        KC.AUDIO_VOL_DOWN,   #Left side clockwise 
-        KC.MEDIA_PREV_TRACK, #Right side counterclockwise                                                          
-        KC.MEDIA_NEXT_TRACK, #Right side clockwise   
+        KC.AUDIO_VOL_UP,     #Left side counterclockwise
+        KC.AUDIO_VOL_DOWN,   #Left side clockwise
+        KC.MEDIA_PREV_TRACK, #Right side counterclockwise
+        KC.MEDIA_NEXT_TRACK, #Right side clockwise
     ],
 ]
 
