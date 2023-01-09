52e94d350e42d124b1b707874eca2076a82f5ac8
janjan <jansmuell@gmail.com>
2023-01-10 00:42:26 +0100

    move led_pos

--- kb.py
+++ kb.py
@@ -6,6 +6,37 @@ from kmk.scanners import DiodeOrientation
 from kmk.scanners.keypad import MatrixScanner
 from kmk.scanners.encoder import RotaryioEncoder
 
+####
+# LED POSITION
+# do not change!
+led_pos_polydactyl = [
+        18, 13, 12,  6,  5,                  26, 27, 33, 34, 39,
+    19, 17, 14, 11,  7,  4,                  25, 28, 32, 35, 38, 40,
+    20, 16, 15, 10,  8,  3,                  24, 29, 31, 36, 37, 41,
+                     9,  2, 1, 0,    21, 22, 23, 30,
+]
+
+led_pos_konrad = [
+        17, 12, 11,  5,  4,                  23, 24, 30, 31, 36,
+    18, 16, 13, 10,  6,  3,                  22, 25, 29, 32, 35, 37,
+    19, 15, 14,  9,  7,  2,                  21, 26, 28, 33, 34, 38,
+                     8,  1, 0,           19, 20, 27,
+]
+
+led_pos_yubitsume = [
+        18, 13, 12,  6,  5,                  24, 25, 31, 32, 37,
+        17, 14, 11,  7,  4,                  23, 26, 30, 33, 36,
+        16, 15, 10,  8,  3,                  22, 27, 29, 34, 35,
+                     9,  2, 1, 0,    19, 20, 21, 28,
+]
+
+led_pos_saegewerk = [
+        17, 12, 11,  5,  4,                  22, 23, 29, 30, 35,
+        16, 13, 10,  6,  3,                  21, 24, 28, 31, 34,
+        15, 14,  9,  7,  2,                  20, 25, 27, 32, 33,
+                     8,  1, 0,           18, 19, 26,
+]
+
 class KMKKeyboard(_KMKKeyboard):
     def __init__(self, klor_rgb, klor_variant, klor_oled, klor_speaker):
         # create and register the scanner(s)
@@ -30,33 +61,24 @@ class KMKKeyboard(_KMKKeyboard):
         self.setup_oled(klor_oled)
         self.setup_speaker(klor_speaker)
 
-
         if klor_rgb == 'peg_rgb':
             from klor_peg_rgb import klor_brightness_limit
 
             self.brightness_limit = klor_brightness_limit
 
             if klor_variant == 'polydactyl':
-                from klor_peg_rgb import led_pos_polydactyl
-
                 self.led_key_pos = led_pos_polydactyl
                 self.num_pixels = len(led_pos_polydactyl)
             
             if klor_variant == 'konrad':
-                from klor_peg_rgb import led_pos_konrad
-
                 self.led_key_pos = led_pos_konrad
                 self.num_pixels = len(led_pos_konrad)
             
             if klor_variant == 'yubitsume':
-                from klor_peg_rgb import led_pos_yubitsume
-
                 self.led_key_pos = led_pos_yubitsume
                 self.num_pixels = len(led_pos_yubitsume)
             
             if klor_variant == 'saegewerk':
-                from klor_peg_rgb import led_pos_saegewerk
-                
                 self.led_key_pos = led_pos_saegewerk
                 self.num_pixels = len(led_pos_saegewerk)        
 
